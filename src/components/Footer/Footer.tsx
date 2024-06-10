d098982c1e85598f9a3fb5092e180078f4661074
Muhammad Ahamd Raza <60704740+theahmadraza@users.noreply.github.com>
2024-06-11 02:33:11 +0500

    never-used-remove

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,6 @@
 import { Container } from "./styles";
 import linkedin from "../../assets/linkedin.svg";
-import githubIcon from "../../assets/github.svg";
 import whatsapp from "../../assets/whatsapp.svg";
-import telegram from "../../assets/telegram.svg";
 import instagramIcon from "../../assets/instagram.svg";
 // import discordIcon from '../../assets/discord.png'
 
